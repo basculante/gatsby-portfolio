12471d706145287b9038f6dc8d32b5814d9cd951
Victor Joh <victor@voicegain.ai>
2021-10-21 21:50:19 -0500

    Hide footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -43,7 +43,7 @@ const Layout = ({ pageTitle, children }: ILayoutProps) => {
       <Navbar mobileMenuControls={mobileMenuControls} toggleMenu={toggleMenu} />
       <div className="children__container">
         {children}
-        <Footer />
+        {/* <Footer /> */}
       </div>
     </div>
   );
