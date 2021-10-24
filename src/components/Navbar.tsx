79ee023b46e135fd2fbb20fa2beb10ac39fec407
Victor Joh <victor@voicegain.ai>
2021-10-24 10:40:10 -0500

    Side menu item link clickable

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,124 +5,129 @@ const Navbar = () => {
   return (
     <nav className="menu">
       <ul>
-        <li className="menu__item" id="home-menu">
-          <Link to="/" className="menu__item-link">
-            <sup>01</sup>Home
-          </Link>
-          <div className="marquee">
-            <div className="marquee__inner-wrap">
-              <div className="marquee__inner" aria-hidden="true">
-                {Array(32)
-                  .fill("home")
-                  .map((title: string, idx: number) => (
-                    <React.Fragment key={`${title}-${idx}`}>
-                      <span>{title}</span>
-                      <div className="marquee__img"></div>
-                    </React.Fragment>
-                  ))}
+        <Link to="/">
+          <li className="menu__item" id="home-menu">
+            <div className="menu__item-link">
+              <sup>01</sup>Home
+            </div>
+            <div className="marquee">
+              <div className="marquee__inner-wrap">
+                <div className="marquee__inner" aria-hidden="true">
+                  {Array(8)
+                    .fill("home")
+                    .map((title: string, idx: number) => (
+                      <React.Fragment key={`${title}-${idx}`}>
+                        <span>{title}</span>
+                        <div className="marquee__img"></div>
+                      </React.Fragment>
+                    ))}
+                </div>
               </div>
             </div>
-          </div>
-        </li>
-        <li className="menu__item" id="work-menu">
-          <Link to="/work" className="menu__item-link">
-            <sup>02</sup>Work
-          </Link>
-          <div className="marquee">
-            <div className="marquee__inner-wrap">
-              <div className="marquee__inner" aria-hidden="true">
-                {Array(32)
-                  .fill("work")
-                  .map((title: string, idx: number) => (
-                    <React.Fragment key={`${title}-${idx}`}>
-                      <span>{title}</span>
-                      <div className="marquee__img"></div>
-                    </React.Fragment>
-                  ))}
+          </li>
+        </Link>
+        <Link to="/work">
+          <li className="menu__item" id="work-menu">
+            <div className="menu__item-link">
+              <sup>02</sup>Work
+            </div>
+            <div className="marquee">
+              <div className="marquee__inner-wrap">
+                <div className="marquee__inner" aria-hidden="true">
+                  {Array(8)
+                    .fill("work")
+                    .map((title: string, idx: number) => (
+                      <React.Fragment key={`${title}-${idx}`}>
+                        <span>{title}</span>
+                        <div className="marquee__img"></div>
+                      </React.Fragment>
+                    ))}
+                </div>
               </div>
             </div>
-          </div>
-        </li>
-        <li className="menu__item" id="resume-menu">
-          <Link to="/resume" className="menu__item-link">
-            <sup>03</sup>Resume
-          </Link>
-          <div className="marquee">
-            <div className="marquee__inner-wrap">
-              <div className="marquee__inner" aria-hidden="true">
-                {Array(32)
-                  .fill("resume")
-                  .map((title: string, idx: number) => (
-                    <React.Fragment key={`${title}-${idx}`}>
-                      <span>{title}</span>
-                      <div className="marquee__img"></div>
-                    </React.Fragment>
-                  ))}
+          </li>
+        </Link>
+        <Link to="/resume">
+          <li className="menu__item" id="resume-menu">
+            <div className="menu__item-link">
+              <sup>03</sup>Resume
+            </div>
+            <div className="marquee">
+              <div className="marquee__inner-wrap">
+                <div className="marquee__inner" aria-hidden="true">
+                  {Array(8)
+                    .fill("resume")
+                    .map((title: string, idx: number) => (
+                      <React.Fragment key={`${title}-${idx}`}>
+                        <span>{title}</span>
+                        <div className="marquee__img"></div>
+                      </React.Fragment>
+                    ))}
+                </div>
               </div>
             </div>
-          </div>
-        </li>
-        <li className="menu__item" id="contact-menu">
-          <a className="menu__item-link" href="mailto:wantechjoh@gmail.com">
-            <sup>04</sup>Email
-          </a>
-          <div className="marquee">
-            <div className="marquee__inner-wrap">
-              <div className="marquee__inner email" aria-hidden="true">
-                {Array(32)
-                  .fill("wantechjoh@gmail.com")
-                  .map((title: string, idx: number) => (
-                    <React.Fragment key={`${title}-${idx}`}>
-                      <span>{title}</span>
-                      <div className="marquee__img"></div>
-                    </React.Fragment>
-                  ))}
+          </li>
+        </Link>
+        <a href="mailto:wantechjoh@gmail.com">
+          <li className="menu__item" id="contact-menu">
+            <div className="menu__item-link">
+              <sup>04</sup>Email
+            </div>
+            <div className="marquee">
+              <div className="marquee__inner-wrap">
+                <div className="marquee__inner email" aria-hidden="true">
+                  {Array(8)
+                    .fill("wantechjoh@gmail.com")
+                    .map((title: string, idx: number) => (
+                      <React.Fragment key={`${title}-${idx}`}>
+                        <span>{title}</span>
+                        <div className="marquee__img"></div>
+                      </React.Fragment>
+                    ))}
+                </div>
               </div>
             </div>
-          </div>
-        </li>
+          </li>
+        </a>
         <div className="menu__item-socials">
-          <li className="menu__item" id="contact-menu">
-            <a className="menu__item-link" href="https://github.com/basculante">
-              GitHub
-            </a>
-            <div className="marquee social">
-              <div className="marquee__inner-wrap">
-                <div className="marquee__inner" aria-hidden="true">
-                  {Array(32)
-                    .fill("github")
-                    .map((title: string, idx: number) => (
-                      <React.Fragment key={`${title}-${idx}`}>
-                        <span>{title}</span>
-                        <div className="marquee__img"></div>
-                      </React.Fragment>
-                    ))}
+          <a href="https://github.com/basculante">
+            <li className="menu__item" id="contact-menu">
+              <div className="menu__item-link">GitHub</div>
+              <div className="marquee social">
+                <div className="marquee__inner-wrap">
+                  <div className="marquee__inner" aria-hidden="true">
+                    {Array(8)
+                      .fill("github")
+                      .map((title: string, idx: number) => (
+                        <React.Fragment key={`${title}-${idx}`}>
+                          <span>{title}</span>
+                          <div className="marquee__img"></div>
+                        </React.Fragment>
+                      ))}
+                  </div>
                 </div>
               </div>
-            </div>
-          </li>
-          <li className="menu__item" id="contact-menu">
-            <a
-              className="menu__item-link"
-              href="https://www.linkedin.com/in/victor-wan-teak-joh-924203ba/"
-            >
-              LinkedIn
-            </a>
-            <div className="marquee social">
-              <div className="marquee__inner-wrap">
-                <div className="marquee__inner" aria-hidden="true">
-                  {Array(32)
-                    .fill("linkedin")
-                    .map((title: string, idx: number) => (
-                      <React.Fragment key={`${title}-${idx}`}>
-                        <span>{title}</span>
-                        <div className="marquee__img"></div>
-                      </React.Fragment>
-                    ))}
+            </li>
+          </a>
+          <a href="https://www.linkedin.com/in/victor-wan-teak-joh-924203ba/">
+            <li className="menu__item" id="contact-menu">
+              <div className="menu__item-link">LinkedIn</div>
+              <div className="marquee social">
+                <div className="marquee__inner-wrap">
+                  <div className="marquee__inner" aria-hidden="true">
+                    {Array(8)
+                      .fill("linkedin")
+                      .map((title: string, idx: number) => (
+                        <React.Fragment key={`${title}-${idx}`}>
+                          <span>{title}</span>
+                          <div className="marquee__img"></div>
+                        </React.Fragment>
+                      ))}
+                  </div>
                 </div>
               </div>
-            </div>
-          </li>
+            </li>
+          </a>
         </div>
       </ul>
     </nav>
