2d69d9ce4fe563082d18fbd73bb3c20e4541b7d1
Victor Joh <victor@voicegain.ai>
2021-10-24 15:10:44 -0500

    Add typescript index.d.ts with img declarations

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "gatsby";
-import { StaticImage } from "gatsby-plugin-image";
 import profileImage from "../images/profile-pic-green.png";
 
 import "../scss/main.scss";
