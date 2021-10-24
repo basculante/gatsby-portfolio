332be0bb0e4f78dc72f47416d21aa881916efbf2
Victor Joh <victor@voicegain.ai>
2021-10-24 15:20:00 -0500

    Background image inline 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,21 +1,25 @@
 import React from "react";
 import { Link } from "gatsby";
-import { StaticImage } from "gatsby-plugin-image";
+import profileImage from "../images/profile-pic-green.png";
+
 import "../scss/main.scss";
 
 const NotFoundPage = () => {
   return (
     <main className="container__404">
       <div>
-        <div className="warning-logo">
-          <StaticImage
+        <div
+          className="warning-logo"
+          style={{ backgroundImage: `url(${profileImage})` }}
+        >
+          {/* <StaticImage
             src="../images/profile-pic-green.png"
             alt="profile picture"
             className="warning-logo-picture"
             height={320}
             width={200}
             layout="fixed"
-          />
+          /> */}
         </div>
         <h1>Are you looking for something else?</h1>
         <h1>
